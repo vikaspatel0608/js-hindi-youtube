d80f2a7f85aac4a78c32438deb7e1ae9cce6a69c
vikaspatel0608 <141300812+vikaspatel0608@users.noreply.github.com>
2024-07-11 13:48:08 +0000

    container config done

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail ="vp4403970@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountID = 2// not allowed
+
+accountEmail = "abc@gmail.com"
+accountPassword = "54321"
+accountCity = "benguluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
